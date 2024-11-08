23e93fc69f89eebf0a98f7a45de3abf380d87b85
ROHITDUBEY777 <166648226+ROHITDUBEY777@users.noreply.github.com>
2024-11-08 11:40:19 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,35 @@
+package daily_questions;
+
+import java.util.Scanner;
+
+public class p1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the first number ");
+        int LL = in.nextInt();
+        System.out.println("ENter the second number ");
+       int UL = in.nextInt();
+       in.close();
+        for(int i=LL;i<=UL;i++){
+        if(checkPrimeNumber(i) == true){
+            System.out.println(i + " ");
+        }
+      }
+    }
+    static boolean checkPrimeNumber(int x)
+    { 
+        if(x==0 || x ==1){
+            System.out.println("false");
+        }
+        for(int i=2;i<x;i++){
+            if(x % i == 0){
+                return false;
+            }
+        }
+        return true;
+       
+      }
+    
+}
+
