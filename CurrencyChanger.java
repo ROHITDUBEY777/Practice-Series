7f5d51adff9254231ae9b7241a23a7861bfa8e81
ROHITDUBEY777 <166648226+ROHITDUBEY777@users.noreply.github.com>
2024-11-08 23:09:12 +0530

    Add files via upload

---
+++ CurrencyChanger.java
@@ -0,0 +1,11 @@
+package daily_questions;
+import java.util.Scanner;
+public class CurrencyChanger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        float a = sc.nextFloat();
+        float  mul = (float)(a/84.37);
+        System.out.println("The Currency in the USD is : " + mul );
+    }
+}
